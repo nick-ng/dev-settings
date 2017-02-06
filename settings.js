ac4b79152eb72696029f0ce62c73669410888f12
Nick Ng <nick-ng@users.noreply.github.com>
2017-02-07 10:18:03 +1300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,15 @@
+{
+  "editor.fontSize": 14,
+  "editor.tabSize": 2,
+  "editor.renderIndentGuides": true,
+  "workbench.editor.showTabCloseButton": false,
+  "workbench.editor.openPositioning": "last",
+  "css.validate": false,
+  "vsicons.hideFolders": true,
+  "vsicons.hideCustomFolderIcons": true,
+  "vsicons.hideIconsInTabs": true,
+  "vsicons.hideIconsEditors": true,
+  "vsicons.useFileAssociations": true,
+  "explorer.enableDragAndDrop": false,
+  "vsicons.dontShowNewVersionMessage": true
+}
