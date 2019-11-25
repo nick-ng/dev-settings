51a358555eb7441704a1a2c213f3b4b14d8a46af
Nick Ng <nick.ng@fergusapp.com>
2019-11-25 13:21:23 +1300

    Put vs code settings in a script.

---
+++ setup-mac.sh
@@ -0,0 +1,4 @@
+# VS Code settings
+echo "Setting up VS Code configuration files."
+cp ./vs-code-settings.json ~/Library/Application\ Support/Code/User/settings.json
+cp ./vs-code-keybinds.json ~/Library/Application\ Support/Code/User/keybindings.json
