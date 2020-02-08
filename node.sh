ed17e156942bf53985bbb1e3aa2812c842bcbef2
Nick Ng <nicholas.cj.ng@gmail.com>
2020-02-08 15:58:45 +1300

    Added NodeJS install script

---
+++ node.sh
@@ -0,0 +1,8 @@
+echo "Install Node Version Manager (nvm)"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install --lts
