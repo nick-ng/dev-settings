457b19fc7df8c37ebdb615431a5d42cf4bdb0175
Nick Ng <nicholas.cj.ng@gmail.com>
2023-01-22 17:02:13 +1300

    temp

---
+++ violent-monkey/youtube-related-hider.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        YouTube Related Hider
+// @namespace   https://github.com/nick-ng/dev-settings/violent-monkey
+// @match       https://www.youtube.com/*
+// @grant       none
+// @version     1.0.0
+// @author      https://github.com/nick-ng
+// @description Hides related videos on YouTube so the result of esports matches aren't spoiled.
+// @downloadURL https://emotes-per-second.pux.one/violent-monkey.js
+// @run-at      document-idle
+// ==/UserScript==
