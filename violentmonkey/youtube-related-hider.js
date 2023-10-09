fd2c89f61af038cb5df4f897c50a4253806ddcff
Nick Ng <nick-ng@users.noreply.github.com>
2023-10-09 17:46:45 +1300

    feat: never show related videos on Mondays (F1)

--- violentmonkey/youtube-related-hider.js
+++ violentmonkey/youtube-related-hider.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/nick-ng/dev-settings/violentmonkey
 // @match       https://www.youtube.com/*
 // @grant       none
-// @version     1.0.2
+// @version     1.0.3
 // @author      https://github.com/nick-ng
 // @description Hides related videos on YouTube so the result of esports matches aren't spoiled.
 // @downloadURL https://raw.githubusercontent.com/nick-ng/dev-settings/master/violentmonkey/youtube-related-hider.js
@@ -11,45 +11,48 @@
 // ==/UserScript==
 
 (() => {
-  const ID = "abc45f16-8c33-4edc-bbb4-be1e5f3dd10e";
+	const ID = "abc45f16-8c33-4edc-bbb4-be1e5f3dd10e";
 
-  for (let i = 0; i < 1; i++) {
-    const tempOldElement = document.getElementById(`${ID}-${i}`);
-    if (tempOldElement) {
-      tempOldElement.remove();
-    }
-  }
+	for (let i = 0; i < 1; i++) {
+		const tempOldElement = document.getElementById(`${ID}-${i}`);
+		if (tempOldElement) {
+			tempOldElement.remove();
+		}
+	}
 
-  /**
-   * helper functions
-   */
-  let counter = 0;
-  const getNextElementId = () => `${ID}-${counter++}`;
+	/**
+	 * helper functions
+	 */
+	let counter = 0;
+	const getNextElementId = () => `${ID}-${counter++}`;
 
-  const makeElement = (tag, parent, text, attributes) => {
-    const tempElement = document.createElement(tag);
-    if (text) {
-      tempElement.textContent = text;
-    }
-    if (parent) {
-      parent.appendChild(tempElement);
-    }
-    if (attributes) {
-      Object.entries(attributes).forEach(([key, value]) => {
-        tempElement.setAttribute(key, value);
-      });
-    }
-    return tempElement;
-  };
+	const makeElement = (tag, parent, text, attributes) => {
+		const tempElement = document.createElement(tag);
+		if (text) {
+			tempElement.textContent = text;
+		}
+		if (parent) {
+			parent.appendChild(tempElement);
+		}
+		if (attributes) {
+			Object.entries(attributes).forEach(([key, value]) => {
+				tempElement.setAttribute(key, value);
+			});
+		}
+		return tempElement;
+	};
 
-  /**
-   * DOM Elements
-   */
-  const headEl = document.getElementsByTagName("head")[0];
-  makeElement(
-    "style",
-    headEl,
-    `
+	/**
+	 * DOM Elements
+	 */
+	const headEl = document.getElementsByTagName("head")[0];
+
+	const isMonday = new Date().getDay() === 1;
+
+	makeElement(
+		"style",
+		headEl,
+		`
     #related div {
       transition-property: opacity;
       transition-duration: 1s;
@@ -60,6 +63,16 @@
     #related:not(:hover) div {
       opacity: 0 !important;
     }
+
+    ${
+			isMonday
+				? `
+    #related div {
+      opacity: 0 !important;
+    }
+    `
+				: ""
+		}
 
     #related::before {
       content: 'Hover to see related videos';
@@ -88,6 +101,6 @@
       display: none !important;
     }
   `,
-    { id: getNextElementId() }
-  );
+		{ id: getNextElementId() }
+	);
 })();
