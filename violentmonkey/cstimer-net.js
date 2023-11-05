e4aa428779b23a4b42a33e895e512b1e693463a9
Nick Ng <nick-ng@users.noreply.github.com>
2023-11-05 21:30:04 +1300

    feat: let you change the tracker number

--- violentmonkey/cstimer-net.js
+++ violentmonkey/cstimer-net.js
@@ -3,30 +3,36 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://cstimer.net/
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      https://github.com/nick-ng
 // @description 9/01/2022, 8:44:06 am
 // @downloadURL https://raw.githubusercontent.com/nick-ng/dev-settings/master/violentmonkey/cstimer-net.js
 // @run-at      document-idle
 // ==/UserScript==
-const checkPlusFifteen = () => {
+const getCurrentNumber = () => {
 	const a = [...document.querySelectorAll("div#stats tbody tr")];
 	const b = a.map((aa) => {
 		const d = aa.getAttribute("data");
 		return d ? parseInt(d, 10) : -1;
 	});
-	const md = Math.max(...b);
+
+	return Math.max(...b);
+};
+
+const checkPlusFifteen = () => {
+	let md = getCurrentNumber();
+
+	const ee = document.querySelector(`div#stats tbody th`);
+
+	ee.addEventListener("click", () => {
+		const newValue = prompt("Enter new value");
+
+		md = getCurrentNumber() - newValue;
+	});
 
 	setInterval(() => {
-		const a2 = [...document.querySelectorAll("div#stats tbody tr")];
-		const b2 = a2.map((aa) => {
-			const d2 = aa.getAttribute("data");
-			return d2 ? parseInt(d2, 10) : -1;
-		});
-		const md2 = Math.max(...b2);
-		const f = Math.max(md2 - md, 0);
+		const f = getCurrentNumber() - md;
 
-		const ee = document.querySelector(`div#stats tbody th`);
 		ee.textContent = `${f}`;
 	}, 500);
 };
