e3a289ba36e076f9cb57969aee46198acd829275
nick-ng <nick.ng@fergus.com>
2024-01-22 14:26:00 +1300

    feat: remove blocking thing on stuff.co.nz

---
+++ violentmonkey/stuff-co-nz.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name        Stuff.co.nz Sign Up Remover
+// @namespace   https://github.com/nick-ng/dev-settings/violentmonkey
+// @match       https://www.stuff.co.nz/*
+// @grant       none
+// @version     1.0.0
+// @author      https://github.com/nick-ng
+// @description Remove the sign up thing
+// @downloadURL https://raw.githubusercontent.com/nick-ng/dev-settings/master/violentmonkey/stuff-co-nz.js
+// @run-at      document-idle
+// ==/UserScript==
+
+(async () => {
+  const selectorQueries = [
+    ".tp-modal",
+    ".tp-backdrop.tp-active",
+  ]
+
+  const main = () => {
+
+    for (const q of selectorQueries) {
+      const els = [...document.querySelectorAll(q)]
+
+      while (els.length > 0) {
+        const el = els.pop()
+        el.remove()
+      }
+    }
+
+    const openEls = document.querySelectorAll(".tp-modal-open")
+
+    for (const el of openEls) {
+      el.classList.remove("tp-modal-open")
+    }
+  }
+
+  for (let n = 0; n < 100000; n++) {
+    let count = 0;
+
+    for (const q of selectorQueries) {
+      const els = [...document.querySelectorAll(q)]
+
+      if (els.length > 0) {
+        count++
+      }
+    }
+
+
+    if (count < selectorQueries.length) {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 100)
+      })
+    } else {
+      main()
+      break;
+    }
+  }
+})();
