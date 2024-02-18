d7fe0dcb1e23bfd06ae0aa9fa559155922a9e8e9
nick-ng <nick.ng@fergus.com>
2024-02-19 10:56:32 +1300

    feat: add progress bar to related videos hider

--- violentmonkey/youtube-related-hider.js
+++ violentmonkey/youtube-related-hider.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/nick-ng/dev-settings/violentmonkey
 // @match       https://www.youtube.com/*
 // @grant       none
-// @version     1.1.0
+// @version     1.2.0
 // @author      https://github.com/nick-ng
 // @description Hides related videos on YouTube so the result of esports matches aren't spoiled.
 // @downloadURL https://raw.githubusercontent.com/nick-ng/dev-settings/master/violentmonkey/youtube-related-hider.js
@@ -49,29 +49,32 @@
 
 	const isMonday = new Date().getDay() === 1;
 
+	const delay = isMonday ? "7" : "1"
+
 	makeElement(
 		"style",
 		headEl,
 		`
-    #related div {
+		#related {
+			position: relative;
+		}
+
+    #related:hover #items {
+			opacity: 1;
       transition-property: opacity;
       transition-duration: 1s;
       transition-timing-function: ease;
-      transition-delay: ${isMonday ? "10" : "1"}s;
+      transition-delay: ${delay}s;
     }
 
-    #related:not(:hover) div {
-      opacity: 0 !important;
+    #related #items {
+      opacity: 0;
 			transition-duration: 0;
 			transition-delay: 0;
     }
 
     #related::before {
-      content: '${
-				isMonday
-					? "Hover 10 seconds to see related videos"
-					: "Hover 2 seconds to see related videos"
-			}';
+      content: 'Hover to see related videos';
       color: white;
       text-shadow:
         -1px -1px 0 #000,
@@ -79,10 +82,26 @@
         -1px 1px 0 #000,
         1px 1px 0 #000;
       font-size: 20px;
+			position: relative;
     }
 
-    #related:hover::before {
-      display: none !important;
+    #related::after {
+      content: ' ';
+			background-color: #888;
+			position: absolute;
+			height: 3px;
+			width: 0;
+			top: 25px;
+			right: 0;
+			transition-property: width;
+      transition-duration: 0;
+      transition-timing-function: linear;
+      transition-delay: 0;
+    }
+
+		#related:hover::after {
+			width: 100%;
+      transition-duration: ${delay}s;
     }
 
     div div.ytp-endscreen-content {
