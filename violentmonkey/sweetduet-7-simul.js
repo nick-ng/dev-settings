319831cdf3c3ed6d4a9c316d043c64328eed201b
nick-ng <nick.ng@fergus.com>
2024-10-14 10:53:51 +1300

    feat: add sweet duet 7 simul trainer

---
+++ violentmonkey/sweetduet-7-simul.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Sweet Duet 7 Simul Trainer
+// @namespace   https://github.com/nick-ng/dev-settings/violentmonkey
+// @include     https://sanya.sweetduet.info/cube/7simul_trainer/
+// @grant       none
+// @version     1.0
+// @author      https://github.com/nick-ng
+// @description Hide memo until you hover
+// @downloadURL https://raw.githubusercontent.com/nick-ng/dev-settings/master/violentmonkey/sweetduet-7-simul.js
+// @run-at      document-idle
+// ==/UserScript==
+
+;(() => {
+  const headEl = [...document.getElementsByTagName("head")][0];
+  const myStyleTag = document.createElement("style")
+  myStyleTag.textContent = `#memo { border: 1px solid #888; border-radius: 16px; box-sizing: border-box; color: #888; background-color: #888; } #memo:hover { color: inherit; background-color: inherit; }`
+  headEl.appendChild(myStyleTag)
+})();
