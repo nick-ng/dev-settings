561c8b4930c921243359d23b07e21774dd09b697
nick-ng <nick.ng@fergus.com>
2024-11-01 16:48:08 +1300

    feat: add a way to choose letter pair by providing search parameter

--- violentmonkey/blddb-net.js
+++ violentmonkey/blddb-net.js
@@ -1,13 +1,15 @@
 // ==UserScript==
-// @name        B L D D B
-// @namespace   https://github.com/nick-ng/dev-settings/violentmonkey
+// @name        B L D D B adjustments
+// @namespace   Violentmonkey Scripts
 // @include     https://blddb.net/corner.html
+// @include     https://blddb.net/corner.html?*
 // @include     https://blddb.net/edge.html
+// @include     https://blddb.net/edge.html?*
 // @grant       none
-// @version     1.0
-// @author      https://github.com/nick-ng
-// @description Hide memo until you hover
-// @downloadURL https://raw.githubusercontent.com/nick-ng/dev-settings/master/violentmonkey/blddb-net.js
+// @version     1.1
+// @author      https://bld.pux.one
+// @description Show move count of commutators and add a query parameter to search for a letter pair
+// @downloadURL https://bld.pux.one/blddb-violentmonkey.js
 // @run-at      document-idle
 // ==/UserScript==
 
@@ -17,56 +19,67 @@
 
 	let inputEl = document.getElementById("cornerinput");
 
-  if (!inputEl) {
-    inputEl = document.getElementById("edgeinput");
-  }
+	if (!inputEl) {
+		inputEl = document.getElementById("edgeinput");
+	}
 
-  if (!inputEl) {
-    console.warn("couldn't find input element")
-    return
-  }
+	if (!inputEl) {
+		console.warn("couldn't find input element");
+		return;
+	}
 
-	inputEl.addEventListener("input", (event) => {
+	const addMoveCount = (counter = 0) => {
+		const tbodyEl = document.querySelector("#table tbody");
+		if (!tbodyEl && counter < 100) {
+			setTimeout(() => {
+				addMoveCount(counter + 1);
+			}, 100);
+
+			return;
+		}
+
 		const elements = [...document.querySelectorAll(`.${ELEMENT_CLASS}`)];
 		for (let i = 0; i < elements.length; i++) {
 			elements[i].remove();
 		}
 
+		const tds = [...tbodyEl.querySelectorAll("td:nth-child(2)")];
+		for (let i = 0; i < tds.length; i++) {
+			const tdEl = tds[i];
+			const algorithm = tdEl.textContent.split(" ").filter((a) => a);
+			let moveCount = 1;
+			for (let j = 1; j < algorithm.length; j++) {
+				if (
+					["U", "D"].includes(algorithm[j - 1][0]) &&
+					["U", "D"].includes(algorithm[j][0])
+				) {
+					// you can do the moves at the same time
+				} else {
+					moveCount = moveCount + 1;
+				}
+			}
+			const moveCountEl = document.createElement("span");
+			moveCountEl.classList.add(ELEMENT_CLASS);
+			moveCountEl.innerHTML = `&nbsp;(${moveCount})`;
+			tdEl.appendChild(moveCountEl);
+		}
+	};
+
+	inputEl.addEventListener("input", (event) => {
 		if (event.currentTarget.value.length === 3) {
-      let counter = 0
-			const addMoveCount = () => {
-				const tbodyEl = document.querySelector("#table tbody");
-				if (!tbodyEl && counter++ < 100) {
-					setTimeout(() => {
-						addMoveCount();
-					}, 100);
-
-					return;
-				}
-
-				const tds = [...tbodyEl.querySelectorAll("td:nth-child(2)")];
-				for (let i = 0; i < tds.length; i++) {
-					const tdEl = tds[i];
-					const algorithm = tdEl.textContent.split(" ").filter((a) => a);
-					let moveCount = 1;
-					for (let j = 1; j < algorithm.length; j++) {
-						if (
-							["U", "D"].includes(algorithm[j - 1][0]) &&
-							["U", "D"].includes(algorithm[j][0])
-						) {
-							// you can do the moves at the same time
-						} else {
-							moveCount = moveCount + 1;
-						}
-					}
-					const moveCountEl = document.createElement("span");
-					moveCountEl.classList.add(ELEMENT_CLASS);
-					moveCountEl.innerHTML = `&nbsp;(${moveCount})`;
-					tdEl.appendChild(moveCountEl);
-				}
-			};
-
-			addMoveCount();
+			addMoveCount(0);
 		}
 	});
+
+	setTimeout(() => {
+		const params = new URLSearchParams(document.location.search);
+		const letterPair = params.get("letter-pair");
+		if (letterPair) {
+			inputEl.value = letterPair;
+			window.algSearch();
+			setTimeout(() => {
+				addMoveCount(0);
+			}, 50)
+		}
+	}, 100);
 })();
