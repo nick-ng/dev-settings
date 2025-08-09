67673c89735cdf32b8d3c989c4dafcf6687cb300
nick-ng <nick-ng@users.noreply.github.com>
2025-08-10 10:26:03 +1200

    fix: correctly check existing start time on start up

--- violentmonkey/youtube-shorts.js
+++ violentmonkey/youtube-shorts.js
@@ -3,63 +3,70 @@
 // @namespace   https://github.com/nick-ng/dev-settings/violentmonkey
 // @match *://www.youtube.com/*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      https://github.com/nick-ng
 // @description Limit the amount of time you spend watching YouTube Shorts
 // @downloadURL https://raw.githubusercontent.com/nick-ng/dev-settings/master/violentmonkey/youtube-shorts.js
 // @run-at      document-idle
 // ==/UserScript==
 
-if (self.navigation) {
-	navigation.addEventListener("navigatesuccess", onUrlChange);
-} else {
-	let u = location.href;
-	new MutationObserver(
-		() => u !== (u = location.href) && onUrlChange()
-	).observe(document, { subtree: true, childList: true });
-}
-
-const STORE_KEY = "pux_youtube_shorts_limit";
-const LIMT_MS = 1000 * 60 * 10; // 10 minutes
-
-const existingStartTime = parseInt(localStorage.getItem(STORE_KEY) || 0, 10);
-let timeoutIds = [];
-
-function onUrlChange() {
-	timeoutIds.forEach((id) => {
-		clearTimeout(id);
-	});
-	timeoutIds = [];
-
-	if (!location.pathname.startsWith("/shorts")) {
-		return;
+(() => {
+	if (self.navigation) {
+		navigation.addEventListener("navigatesuccess", onUrlChange);
+	} else {
+		let u = location.href;
+		new MutationObserver(
+			() => u !== (u = location.href) && onUrlChange()
+		).observe(document, { subtree: true, childList: true });
 	}
 
-	let elapsedTime = Date.now() - existingStartTime;
-	if (elapsedTime > LIMT_MS * 3) {
-		elapsedTime = 0;
-		localStorage.setItem(STORE_KEY, existingStartTime.toString());
-	}
+	const STORE_KEY = "pux_youtube_shorts_limit";
+	const LIMT_MS = 1000 * 60 * 10; // 10 minutes
 
-	const remainingTime = LIMT_MS - elapsedTime;
-	console.log(`${remainingTime / 1000} seconds left`);
-	timeoutIds.push(
-		setTimeout(() => {
-			console.log("time is up");
-			if (location.pathname.startsWith("/shorts")) {
-				window.location = "https://github.com/nick-ng/dev-settings#readme";
-			}
-		}, remainingTime)
-	);
+	let timeoutIds = [];
 
-	for (let i = 0; i > remainingTime; i += 30000) {
-		const message = `${i / 1000} seconds left`;
+	function onUrlChange() {
+		timeoutIds.forEach((id) => {
+			clearTimeout(id);
+		});
+
+		const existingStartTime = parseInt(
+			localStorage.getItem(STORE_KEY) || 0,
+			10
+		);
+		timeoutIds = [];
+
+		if (!location.pathname.startsWith("/shorts")) {
+			return;
+		}
+
+		let elapsedTime = Date.now() - existingStartTime;
+		if (elapsedTime > LIMT_MS * 3) {
+			elapsedTime = 0;
+			localStorage.setItem(STORE_KEY, existingStartTime.toString());
+		}
+
+		const remainingTime = LIMT_MS - elapsedTime;
+		console.info(`${remainingTime / 1000} seconds left`);
 		timeoutIds.push(
 			setTimeout(() => {
-				console.log(message);
-			}, remainingTime - i)
+				console.info("time is up");
+				if (location.pathname.startsWith("/shorts")) {
+					window.location =
+						"https://github.com/nick-ng/dev-settings#readme";
+				}
+			}, remainingTime)
 		);
-	}
-}
 
-onUrlChange();
+		for (let i = 0; i > remainingTime; i += 30000) {
+			const message = `${i / 1000} seconds left`;
+			timeoutIds.push(
+				setTimeout(() => {
+					console.info(message);
+				}, remainingTime - i)
+			);
+		}
+	}
+
+	onUrlChange();
+})();
