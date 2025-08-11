bd6f6dfb169194050df1bf28e5e12eb2a284a2fa
nick-ng <nick.ng@fergus.com>
2025-08-12 10:38:24 +1200

    feat: script to set sensitivity of work mouse

---
+++ linux/mouse-sensitivity.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# SATECHI mouse at work
+# Set to 3200 dpi - purple
+XINPUT_LINE="$(xinput --list | grep 'PixArt OpticalMouse  ')"
+if [[ $XINPUT_LINE ]]; then
+		echo "$XINPUT_LINE"
+		TEMP="${XINPUT_LINE#*id=}"
+		MOUSE_ID="${TEMP%	*}"
+		echo "$MOUSE_ID"
+
+		# last item (2.00) is number of input events to move 1 pixel i.e. "inverse" sensitivity
+		xinput --set-prop "$MOUSE_ID" 'Coordinate Transformation Matrix' 1.00 0.00 0.00 0.00 1.00 0.00 0.00 0.00 2.00
+fi
