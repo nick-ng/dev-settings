991e00c37f8fb72af18eb07baf33e93b56b88ab8
nick-ng <nick.ng@fergus.com>
2025-08-18 14:44:51 +1200

    feat: change shorts limit. add separate rest time for shorts

--- violentmonkey/youtube-shorts.js
+++ violentmonkey/youtube-shorts.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/nick-ng/dev-settings/violentmonkey
 // @match *://www.youtube.com/*
 // @grant       none
-// @version     1.4
+// @version     1.5
 // @author      https://github.com/nick-ng
 // @description Limit the amount of time you spend watching YouTube Shorts
 // @downloadURL https://raw.githubusercontent.com/nick-ng/dev-settings/master/violentmonkey/youtube-shorts.js
@@ -21,7 +21,8 @@
 	}
 
 	const STORE_KEY = "pux_youtube_shorts_limit";
-	const LIMIT_MS = 1000 * 60 * 10; // 10 minutes
+	const LIMIT_MS = 1000 * 60 * 8; // 8 minutes
+	const RESET_MS = 1000 * 60 * 30; // 30 minutes
 	const REDIRECT_URL = "https://youtu.be/raHVKhS-A94";
 
 	let timeoutIds = [];
@@ -43,10 +44,11 @@
 		);
 
 		let elapsedTime = Date.now() - existingStartTime;
-		if (elapsedTime > LIMIT_MS * 3) {
+		if (elapsedTime > RESET_MS) {
 			elapsedTime = 0;
 			localStorage.setItem(STORE_KEY, Date.now().toString());
 		} else if (elapsedTime >= LIMIT_MS) {
+			// redirect on next navigation
 			console.info("time is up");
 			if (location.pathname.startsWith("/shorts")) {
 				window.location = REDIRECT_URL;
@@ -65,7 +67,7 @@
 					if (j === 0) {
 						const day = new Date().getDay();
 						if (day === 0 || day === 6) {
-							// if Sunday (0) or Saturday (6), redirect in LIMIT_MS
+							// if Sunday (0) or Saturday (6), redirect in LIMIT_MS, 2 * LIMIT_MS total
 							timeoutIds.push(
 								setTimeout(() => {
 									console.info("double time up");
