15f4ed78affa2085505e7c21512f8d41d9d6e5c4
pux <nick-ng@users.noreply.github.com>
2025-09-28 17:34:22 +1300

    feat: maximise gnome terminal when first opened ;

--- gnome/toggle-terminal.sh
+++ gnome/toggle-terminal.sh
@@ -8,4 +8,13 @@ if ps aux | grep -v grep | grep gnome-terminal > /dev/null; then
 	fi
 else
 	gnome-terminal &
+	for (( i = 0 ; i < 1000 ; i++ )); do
+		if [ -n "$(xdotool search --class ^gnome-terminal$)" ]; then
+			xdotool search --class ^gnome-terminal$ windowactivate --sync
+			xdotool key --clearmodifiers super+F11
+			break;
+		fi
+
+		sleep 0.2s
+	done
 fi
