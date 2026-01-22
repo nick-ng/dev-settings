c4b74bcfd6355df3e2219bfecdca145f5402b8e0
nick-ng <nick-ng@users.noreply.github.com>
2026-01-23 11:29:24 +1300

    feat: update hammerspoon script

--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -1,3 +1,11 @@
 hs.hotkey.bind({ "cmd", "ctrl" }, "1", function()
 	hs.application.launchOrFocus("Terminal")
 end)
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "2", function()
+	hs.application.launchOrFocus("Firefox")
+end)
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "3", function()
+	hs.application.launchOrFocus("VSCodium")
+end)
